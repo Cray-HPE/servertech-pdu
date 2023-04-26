b76ea0ad05d4eafff55c32fdc645affd0d451031
Michael Jendrysik <77011943+mjendrysik-hpe@users.noreply.github.com>
2023-04-26 07:59:46 -0700

    CASMHMS-5103 Servertech power control (#1)
    * Initial code check-in of the server tech control app. Includes CODEOWNERS, PR template, and requirements.txt

---
+++ pdu/jaws.py
@@ -0,0 +1,263 @@
+#!/usr/bin/python3
+
+# MIT License
+#
+# (C) Copyright [2023] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+""" Low level communication with iPDU JAWS API
+
+Makes the http calls using the requests module to the JAWS API of the Server
+Tech iPDU. API calls are limited to those required for status query and patching
+of the power status for groups and outlets.
+
+Typical usage example:
+    jaws = Jaws(hostname|IPv4|IPv6, username, password)
+    outlet_status = jaws.get_outlet_status_all()
+    groups = jaws.get_group_information()
+
+
+"""
+
+import json
+from http import HTTPStatus
+import requests
+import urllib3
+
+class URLs:
+    """ Jaws API URLs """
+    GROUP_CONTROL = 'jaws/control/groups'
+    GROUP_MONITOR = 'jaws/monitor/groups'
+    OUTLET_CONTROL = 'jaws/control/outlets'
+    OUTLET_MONITOR = 'jaws/monitor/outlets'
+
+class Jaws:
+    """
+    Definition of JAWS communication interfaces for a Server Tech iPDU.
+
+    Attributes:
+        _host(string):
+            IPv4, IPv6, or hostname of the iPDU.
+        _basic_auth(HTTPBasicAuth):
+            Authorization used with requests module.
+        _headers(dictionary):
+            HTTP send headers.
+        _base_url(string):
+            Base https URL string
+
+    Modules:
+        configure_basic_auth(string, string):
+            Converts a username and password into an HTTPBasicAuth structure.
+        generate_header():
+            Creates a basic header for all HTTPS communication and stores it.
+        get_outlet_status_all() -> dict:
+            Returns a dictionary containing all the outlets and their status
+            from the iPDU.
+        get_group_information() -> dict:
+            Returns a dictionaly containing all the groups of the iPDU.
+        send_outlet_power_command(string, string) -> int:
+            Sends a PATCH operation to the iPDU targeting a single outlet with
+            an on/off/reboot payload.
+        send_group_power_command(string, string) -> int:
+            Sends a PATCH operation to the iPDU targeting a single group with
+            an on/off/reboot payload.
+    """
+    def __init__(self, host: str, user: str, passwd: str) -> None:
+        self._host = host
+        self._basic_auth = None
+        self._headers = ''
+        self._base_url = 'https://%s' % self._host
+        self.configure_basic_auth(user, passwd)
+        self.generate_header()
+
+    def configure_basic_auth(self, user: str, passwd: str):
+        """
+        Transform user and passwd into a requets basic auth structure.
+
+        Parameters:
+            user (string): Username for the Jaws API
+            passwd (string): Password for the user at the Jaws API
+
+        Returns:
+            None
+        """
+        self._basic_auth = requests.auth.HTTPBasicAuth(user, passwd)
+
+    def generate_header(self):
+        """
+        Create a generic header for all HTTP Jaws operations
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        self._headers = {
+                'cache-control': 'no-cache',
+                'Content-Type': 'application/json',
+            }
+
+    def get_outlet_status_all(self) -> dict:
+        """
+        Query the Jaws API for outlet status for target outlet.
+
+        Parameters:
+            None
+
+        Returns:
+            A dictionary containing all the outlets and their status
+            information.
+        """
+        # SSL verification is not used which results in a
+        # InsecureRequestWarning. The following line disables only the
+        # IsnsecureRequestWarning.
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        url = '%s/%s' % (self._base_url, URLs.OUTLET_MONITOR)
+        rsp = requests.get(
+                url = url,
+                auth = self._basic_auth,
+                headers = self._headers,
+                verify = False
+            )
+
+        if not rsp.text:
+            return None
+
+        if rsp.status_code >= HTTPStatus.MULTIPLE_CHOICES:
+            print('get_outlet_status_all: Jaws call returned %d.' %
+                    rsp.status_code)
+
+        return rsp.text
+
+    def get_group_information(self) -> dict:
+        """
+        Query the Jaws API for outlets associated with a group.
+
+        Parameters:
+            None
+
+        Returns:
+            A dictionary containing the group definitions and the outlets
+            associated with them.
+        """
+        # SSL verification is not used which results in a
+        # InsecureRequestWarning. The following line disables only the
+        # IsnsecureRequestWarning.
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        url = '%s/%s' % (self._base_url, URLs.GROUP_MONITOR)
+
+        rsp = requests.get(
+                url = url,
+                auth = self._basic_auth,
+                headers = self._headers,
+                verify = False
+            )
+
+        if not rsp.text:
+            return None
+
+        if rsp.status_code >= HTTPStatus.MULTIPLE_CHOICES:
+            print('get_group_information: Jaws call returned %d.' %
+                    rsp.status_code)
+            return None
+
+        return rsp.text
+
+    def send_outlet_power_command(self, operation: str, outlet: str) -> int:
+        """
+        Send the power operation to the target outlet.
+
+        Parameters:
+            operation(string):
+                on/off/reboot command.
+            outlet(string):
+                Target outlet in the iPDU.
+
+        Returns:
+            0 on success, 1 on error
+        """
+        # SSL verification is not used which results in a
+        # InsecureRequestWarning. The following line disables only the
+        # IsnsecureRequestWarning.
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        url = '%s/%s/%s' % (self._base_url, URLs.OUTLET_CONTROL, outlet)
+
+        payload = {
+            'control_action': operation
+        }
+
+        rsp = requests.patch(
+            url = url,
+            auth = self._basic_auth,
+            headers = self._headers,
+            data = json.dumps(payload),
+            verify = False
+        )
+
+        if rsp.status_code >= HTTPStatus.MULTIPLE_CHOICES:
+            print('send_outlet_power_command: (%d) Failed to send \'%s\' to ' \
+                    'outlet %s in %s' %
+                    (rsp.status_code, operation, outlet, self._host))
+            return 1
+
+        return 0
+
+    def send_group_power_command(self, operation: str, group: str) -> int:
+        """
+        Send the power operation to the target group.
+
+        Parameters:
+            operation(string):
+                on/off/reboot command.
+            group(string):
+                Target outlet in the iPDU.
+
+        Returns:
+            0 on success, 1 on error
+        """
+        # SSL verification is not used which results in a
+        # InsecureRequestWarning. The following line disables only the
+        # IsnsecureRequestWarning.
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        url = '%s/%s/%s' % (self._base_url, URLs.GROUP_CONTROL, group)
+
+        payload = {
+            'control_action': operation
+        }
+
+        rsp = requests.patch(
+            url = url,
+            auth = self._basic_auth,
+            headers = self._headers,
+            data = json.dumps(payload),
+            verify = False
+        )
+
+        if rsp.status_code >= HTTPStatus.MULTIPLE_CHOICES:
+            print('send_group_power_command: (%d) Failed to send \'%s\' to ' \
+                    'group %s in %s' %
+                    (rsp.status_code, operation, group, self._host))
+            return 1
+
+        return 0
---
+++ pdu/pdu.py
@@ -0,0 +1,224 @@
+#!/usr/bin/python3
+
+# MIT License
+#
+# (C) Copyright [2023] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Allows interaction with a Server Tech iPDU
+
+Provides a limited number of interfaces for interacting with the Server Tech
+iPDU. These actions are limited to those required to get status and send power
+commands to the iPDU. All calls to JAWS have a basic retry mechanism for the
+expected communications issues with the iPDU controller.
+
+Typical usage example:
+    pdu = PDU(hostname|IPv4|IPv6, username, password)
+    groups = pdu.get_group_information()
+    outlet_status = pdu.get_outlet_status_all()
+
+"""
+
+import json
+import time
+from pdu.jaws import Jaws
+
+class PDU:
+    """
+    Definition of a Server Tech iPDU
+
+    Attributes:
+        _host(string):
+            IPv4, IPv6, or hostname of target iPDU.
+        _user(string):
+            Username to access JAWS interface of iPDU.
+        _jaws(object):
+            Underlying JAWS communication implementation.
+
+    Methods:
+        get_host() -> string:
+            Returns hostname/IP of the iPDU.
+        get_outlet_status_all() -> dict:
+            Returns a dictionary containing all the outlets and their status
+            from the iPDU.
+        get_group_information() -> dict:
+            Returns a dictionaly containing all the groups of the iPDU.
+        do_outlet_power_control(list) -> int:
+            Sends power operations from the list of outlets to the iPDU.
+        do_group_power_control(list) -> int:
+            Sends power operations from the list of groups to the iPDU.
+    """
+    def __init__(self, host: str, user: str, passwd: str) -> None:
+        self._host = host
+        self._user = user
+        self._jaws = Jaws(host, user, passwd)
+
+    def get_host(self) -> str:
+        """ Return hostname/IP """
+        return self._host
+
+    def get_outlet_status_all(self) -> dict:
+        """
+        The status of all the outlets in the iPDU. Will retry the JAWS call if
+        there is an error response or if the JSON payload returned is invalid.
+
+        Parameters:
+            None
+
+        Returns:
+            A dictionary containing all the outlets and their status
+            information.
+        """
+        outlet_status = None
+        retries = 0
+        while True:
+            if retries > 5:
+                print('get_outlet_status_all: exceeded retries for %s, ' \
+                        'giving up' % self._host)
+                return None
+            try:
+                rsp = self._jaws.get_outlet_status_all()
+
+                if rsp is None:
+                    print('get_outlet_status_all: None returned from %s, ' \
+                            'retrying ...' % self._host)
+                    time.sleep(1)
+                    retries += 1
+                    continue
+
+                outlet_status = json.loads(rsp)
+                break
+            except json.decoder.JSONDecodeError:
+                print('get_outlet_status_all: JSON decode Failed from %s, ' \
+                        'retrying ...' % self._host)
+                time.sleep(1)
+                retries += 1
+
+        return outlet_status
+
+    def get_group_information(self) -> dict:
+        """
+        The group definitions of the iPDU. Will retry the JAWS call if there is
+        an error response or if the JSON payload returned is invalid.
+
+        Parameters:
+            None
+
+        Returns:
+            A dictionary containing the group definitions and the outlets
+            associated with them.
+        """
+        group_info = None
+        retries = 0
+        while True:
+            if retries > 5:
+                print('get_group_information: exceeded retries for %s, ' \
+                        'giving up' % self._host)
+                return None
+            try:
+                rsp = self._jaws.get_group_information()
+
+                if rsp is None:
+                    print('get_group_information: None returned from %s, ' \
+                            'retrying ...' % self._host)
+                    time.sleep(1)
+                    retries += 1
+                    continue
+
+                group_info = json.loads(rsp)
+                break
+            except json.decoder.JSONDecodeError:
+                print('get_group_information: JSON decode failed from %s, ' \
+                            'retrying ...' % self._host)
+                time.sleep(1)
+                retries += 1
+
+        return group_info
+
+    def do_outlet_power_control(self, outlets: list):
+        """
+        Send a power operation to the list of outlets. Will retry the JAWS call
+        if there is an error response or if the JSON payload returned is
+        invalid.
+
+        Parameters:
+            outlets(list):
+                List of dictionaries that contain an outlet name and the
+                operation to be performed.
+
+        Returns:
+            None
+        """
+        for outlet in outlets:
+            retries = 0
+            while True:
+                if retries >= 5:
+                    print('do_outlet_power_control: %s exceeded retries for ' \
+                            'outlet %s at %s, giving up' %
+                            (outlet['operation'], outlet['name'], self._host))
+                    break
+
+                retval = self._jaws.send_outlet_power_command(outlet['operation'], outlet['name'])
+
+                if retval == 0:
+                    print('Success, %s sent for outlet %s at ' \
+                            '%s' % (outlet['operation'], outlet['name'],
+                                    self._host))
+                    break
+                else:
+                    print('do_outlet_power_control: %s failed to send for ' \
+                            'outlet %s at %s, retrying ...' %
+                            (outlet['operation'], outlet['name'], self._host))
+                    time.sleep(1)
+                    retries += 1
+
+    def do_group_power_control(self, groups: list):
+        """
+        Send a power operation to the list of groups. Will retry the JAWS call
+        if there is an error response or if the JSON payload returned is
+        invalid.
+
+        Parameters:
+            groups(list):
+                List of dictionaries that contain a group name and the operation
+                to be performed.
+        """
+        for group in groups:
+            retries = 0
+            while True:
+                if retries >= 5:
+                    print('do_group_power_control: %s exceeded retries for ' \
+                            'group %s at %s, giving up' %
+                            (group['operation'], group['name'], self._host))
+                    break
+
+                retval = self._jaws.send_group_power_command(group['operation'], group['name'])
+
+                if retval == 0:
+                    print('Success, %s sent for group %s at %s' %
+                            (group['operation'], group['name'], self._host))
+                    break
+                else:
+                    print('do_group_power_control: %s failed to send for ' \
+                            'group %s at %s, retrying ...' %
+                            (group['operation'], group['name'], self._host))
+                    time.sleep(1)
+                    retries += 1
